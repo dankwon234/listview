40b98eba197de9d4e301e4d16a188940b94a1684
fullstack360 <info@fullstack360.com>
2016-04-24 12:12:42 -0400

    scroll view with page control

--- Listview/ViewController.swift
+++ Listview/ViewController.swift
@@ -8,17 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var nameField: UITextField!
+    @IBOutlet var imagesScroll: UIScrollView!
+    @IBOutlet var pageControl: UIPageControl!
     var namesArray = Array<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        let firstSlide = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        firstSlide.backgroundColor = UIColor.blueColor()
+        self.imagesScroll.addSubview(firstSlide)
+        
+        let secondSlide = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
+        secondSlide.backgroundColor = UIColor.greenColor()
+        self.imagesScroll.addSubview(secondSlide)
+        
+        self.imagesScroll.contentSize = CGSize(width: 400, height: 0)
+        self.imagesScroll.delegate = self
+        
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.x
+//        print("scrollViewDidScroll: \(offset)")
+        
+        if (offset > 100){
+            self.view.backgroundColor = UIColor.redColor()
+        }
+        else {
+            self.view.backgroundColor = UIColor.lightGrayColor()
+        }
+        
     }
 
-    @IBAction func addName(){
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset.x
+        print("scrollViewDidEndDecelerating: \(offset)")
         
+        if (offset == 0){
+            print("FIRST PAGE")
+            self.pageControl.currentPage = 0
+        }
+
+        if (offset == 200){
+            print("SECOND PAGE")
+            self.pageControl.currentPage = 1
+        }
+
+    }
+    
+    
+    @IBAction func addName(){
         let name = self.nameField.text
         if (name?.characters.count == 0){
             return // nothing entered, ignore
@@ -32,7 +77,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
