7792035bc201633be8ef944c18bf6f57966b1c54
fullstack360 <info@fullstack360.com>
2016-04-24 12:40:05 -0400

    add third slide

--- Listview/ViewController.swift
+++ Listview/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.lightGrayColor()
         
         
         let firstSlide = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
@@ -26,23 +27,29 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         let secondSlide = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
         secondSlide.backgroundColor = UIColor.greenColor()
         self.imagesScroll.addSubview(secondSlide)
-        
-        self.imagesScroll.contentSize = CGSize(width: 400, height: 0)
+
+        let thirdSlide = UIView(frame: CGRect(x: 400, y: 0, width: 200, height: 200))
+        thirdSlide.backgroundColor = UIColor.orangeColor()
+        self.imagesScroll.addSubview(thirdSlide)
+
+        self.imagesScroll.contentSize = CGSize(width: 600, height: 0)
         self.imagesScroll.delegate = self
         
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         let offset = scrollView.contentOffset.x
-//        print("scrollViewDidScroll: \(offset)")
-        
-        if (offset > 100){
-            self.view.backgroundColor = UIColor.redColor()
+
+        if (offset < 100){
+            self.pageControl.currentPage = 0
         }
-        else {
-            self.view.backgroundColor = UIColor.lightGrayColor()
+        else if (offset < 300){ // second page
+            self.pageControl.currentPage = 1
         }
-        
+        else { // last page
+            self.pageControl.currentPage = 2
+        }
+
     }
 
     
